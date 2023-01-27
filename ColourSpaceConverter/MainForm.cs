876a6f846892ccb56d6d1175c442f19c95209e73
grishagr9 <56480294+grishagr9@users.noreply.github.com>
2023-01-28 02:52:41 +0300

    Add files via upload

---
+++ ColourSpaceConverter/MainForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColourSpaceConverter
+{
+    public partial class MainForm : Form
+    {
+        private bool formOpened = false;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rgbToGrayScaleButton_Click(object sender, EventArgs e)
+        {
+            var rgbToGrayScaleForm = new RGBToGrayScaleForm();
+            rgbToGrayScaleForm.ShowDialog();
+        }
+
+        private void rgbChannelsExtractionButton_Click(object sender, EventArgs e)
+        {
+            var rgbChannelsExtractionForm = new RGBChannelsExtractionForm();
+            rgbChannelsExtractionForm.ShowDialog();
+        }
+
+        private void rgbToHsvButton_Click(object sender, EventArgs e)
+        {
+            var rgbToHsvForm = new RGBToHSVForm();
+            rgbToHsvForm.ShowDialog();
+        }
+    }
+}
